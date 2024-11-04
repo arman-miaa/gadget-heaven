cee895eef5dacc7d7834870c3fe8374a4d712408
Arman <mdarman365506@gmail.com>
2024-11-04 06:42:11 +0600

    added product details section

--- src/Pages/ProductDetails.jsx
+++ src/Pages/ProductDetails.jsx
@@ -1,9 +1,32 @@
+import Hero from "../components/Hero";
 
 const ProductDetails = () => {
     return (
-        <div>
-            <h1>Product details </h1>
+      <div>
+        <div className="bg-[#9538E2] relative  pb-32 text-white -mt-[48px] text-center ">
+          <h1 className="font-bold text-5xl pt-8 ">
+            Product Details
+          </h1>
+          <p className="my-6">
+            Explore the latest gadgets that will take your experience to <br />{" "}
+            the next level. From smart devices to the coolest accessories, we
+            have it all!
+          </p>
+         
+
+          <Hero></Hero>
         </div>
+        <div className="mb-16 w-full  -mt-28 sticky">
+          <div className="border-2 w-3/4 mx-auto p-4 rounded-xl">
+            <img
+              className=" w-full mx-auto rounded-2xl  text-center  h-[363px] "
+              src="/banner.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+        <h1>Product Details Page...</h1>
+      </div>
     );
 };
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,13 @@
 
 const Footer = () => {
     return (
-      <div>
-        <footer className="footer bg-base-300 text-base-content p-10">
+      <div className="bg-base-200 pt-16">
+        <div className="text-center">
+          <h3 className="text-3xl font-bold">Gadget Heaven</h3>
+          <p>Leading the way in cutting-edge technology and innovation.</p>
+        </div>
+        <hr className="mt-8" />
+        <section className="footer  text-base-content p-10">
           <nav>
             <h6 className="footer-title">Services</h6>
             <a className="link link-hover">Product Support</a>
@@ -22,7 +27,7 @@ const Footer = () => {
             <a className="link link-hover">Privacy Policy</a>
             <a className="link link-hover">Cookie Policy</a>
           </nav>
-        </footer>
+        </section>
       </div>
     );
 };
